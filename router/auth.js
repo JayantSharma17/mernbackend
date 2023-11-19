e7f4ab46d73e67957ef28e5c188a70c7a609a03e
Jayant Sharma <105975938+JayantSharma17@users.noreply.github.com>
2023-11-19 12:19:29 +0530

    image path change3

--- router/auth.js
+++ router/auth.js
@@ -82,7 +82,7 @@ router.post('/signin', async (req, res) => {
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,path.join('./Images'))
+        cb(null,'/public/Images')
     },
     filename:(req,file,cb)=>{
         cb(null,file.fieldname + "_" + Date.now() + path.extname(file.originalname))
